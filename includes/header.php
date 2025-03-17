814e7a16752eadd757164651715e26199cd20562
Placathose <bruceplacathose@gmail.com>
2025-03-16 23:59:13 -0400

    init

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Museum CMS</h1>
+
+  <div>
+    <a href="logout.php">Logout</a>
+    <a href="dashboard.php">Dashboard</a>
+  </div>
+  
+</body>
+</html>
